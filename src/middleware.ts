9f8a61c70152443eae7870b30ff9a2fd0430c4a2
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2024-10-07 22:11:36 +0530

    chore: :construction: Update Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,14 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!auth().userId && isProtectedRoute(req)) {
+    // Add custom logic to run before redirecting
+
+    return auth().redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
