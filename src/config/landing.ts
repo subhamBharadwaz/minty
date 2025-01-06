3f8142f56fdc6e278d251e71cde1a9c55ab8d30d
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2025-01-06 13:26:33 +0530

    feat: :sparkles: Site Configurations

--- src/config/landing.ts
+++ src/config/landing.ts
@@ -2,11 +2,15 @@ import { NavItem } from "@/types";
 
 export const landingConfig: NavItem[] = [
   {
+    title: "How it Works",
+    href: "#how-it-works",
+  },
+  {
     title: "Features",
-    href: "/#features",
+    href: "#features",
   },
   {
     title: "Pricing",
-    href: "/#pricing",
+    href: "#pricing",
   },
 ];
---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Minty",
+  description:
+    "Take control of your finances with Minty, the smart and simple finance tracker app. Budget, track expenses, and achieve your financial goals effortlessly. Your path to financial freedom starts here!",
+  url: "https://minty.subhambharadwaz.com",
+  ogImage: "https://minty.subhambharadwaz.com/og.jpg",
+  links: {
+    twitter: "https://twitter.com/webhashira",
+    github: "https://github.com/subhamBharadwaz",
+    linkedin: "https://www.linkedin.com/in/subham-bharadwaz-5a9792197/",
+  },
+};
