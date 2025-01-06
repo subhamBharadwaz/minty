4b0caff9780ed4c8065f4cb7a2abe64cc46e7491
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2025-01-06 13:34:16 +0530

    chore: :construction: Update Next Theme Code

--- src/components/providers/theme.tsx
+++ src/components/providers/theme.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps,
+} from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
