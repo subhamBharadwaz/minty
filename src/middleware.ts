a431858ac66a9f6ae5844c6255662ef369af83e5
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2025-01-06 13:27:03 +0530

    chore: :zap: Updated Middleware with Latest Clerk checks

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  if (!auth().userId && isProtectedRoute(req)) {
-    // Add custom logic to run before redirecting
-
-    return auth().redirectToSignIn();
-  }
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
 });
 
 export const config = {
